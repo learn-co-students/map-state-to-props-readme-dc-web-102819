3cbacceb681440fe66404c1c06c0e7eeb1c40563
jdromero88 <jdromerov88@gmail.com>
2020-02-20 15:44:42 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import {connect} from 'react-redux'
 
 class App extends Component {
 
@@ -20,5 +21,7 @@ class App extends Component {
     );
   }
 };
-
-export default App;
+const mapSateToProps = state => {
+  return {items: state.items}
+}
+export default connect(mapSateToProps)(App);
